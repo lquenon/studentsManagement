66fc958199b61fa0478ee1aeb0142bb26607c510
lquenon <lquenon@gmail.com>
2024-07-15 08:58:32 +0200

    Ajout des commentaires

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+# initialisation tkinter
 import tkinter as tk
 from tkinter import messagebox
 
 root = tk.Tk()
 root.title("Système de gestion d'étudiants")
 root.geometry("1200x780")
+
+# exécution de la boucle principale
 root.mainloop()
